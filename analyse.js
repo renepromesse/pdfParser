43cb62a9ace578a88517777e52da604fe77acd97
Rene Promesse <nrenejordan1@gmail.com>
2022-01-15 23:58:19 +0200

    added the pursing endpoints

---
+++ analyse.js
@@ -0,0 +1,77 @@
+{
+    "pdfText": "\n\n
+    2020-2021Année académique :\n
+    Principles of Accounting I38.891ACCT 112EC * \n
+    Mathématiques de l'Informatique311.401AMAT 111  \n
+    Méthodes d'Étude Et De Recherche211.001EDRM 113  * \n
+    General English317.201ENGL 115  * \n
+    Microcomputer Applications317.901INSY 116  * \n
+    Introduction à l étude de la Bible211.201RELB 116  * \n
+    Multivariable Calculus & Differential Equations49.002MATH 126EC \n
+    Logic et Circuits fondamentaux313.202MATH 127  \n
+    Introduction to Computer Programming415.502INSY 227  \n
+    Entretien Des Ordinateurs315.802INSY 214  \n
+    Principles of Accounting I313.202ACCT 112  * \n
+    2021-2022Année académique :\n
+    Academic English Writing31ENGL 124* \n
+    Système de Gestion de Base  des Données31INSY 217 \n
+    Programmation avec C41INSY 228 \n
+    Computer Networks41INSY 229 \n
+    Theory of Computation31SENG 224 \n
+    Statistiques Descriptive31STAT 122* \n
+    UNIVERSITE  ADVENTISTE D'AFRIQUE CENTRALE\n
+    B.P.  2461 KIGALI, RWANDA\n
+    RELEVE DE NOTES\n
+    23870Ntirenganya Rene PromesseNom\n
+    Faculté\n
+    No. Matricule\n
+    Option\n
+    Mineur \n
+    Software EngineeringInformation Technology\n:\n::\n:\n
+    SemSigleCoursCr .NoteCodes\n
+    ActiveReports Evaluation. Copyright 2002-2005 (c) Data Dynamics, Ltd. All Rights Reserved.\n\n
+    UNIVERSITE  ADVENTISTE D'AFRIQUE CENTRALE\n
+    B.P.  2461 KIGALI, RWANDA\n
+    RELEVE DE NOTES\n
+    23870Ntirenganya Rene PromesseNom\n
+    Faculté\n
+    No. Matricule\n
+    Option\n
+    Mineur \n
+    Software EngineeringInformation Technology\n:\n::\n:\n
+    SemSigleCoursCr .NoteCodes\n
+    UNOFFICIAL\nRépartition des crédits\n
+    Cours généraux\n
+    Cours d'option\n
+    Cours du mineur\n
+    Cours transférés\n
+    Cours complémentaires\n:\n:\n:\n:\n:\n0\n19\n27\n0\n0\n
+    At Kigali, on\n
+    Mboyo Mukunde Sylvie\n
+    13/01/2022\n13.62\n12.89\n
+    Moyennes\n
+    Moyenne générale cumulative\n
+    Moyenne d'option cumulative\n:\n:\n
+        : Cours réussi\n
+        EC  : Cours échoué\n
+        TF  : Cours transféré\n
+        W   : Cours retiré\n
+        *   : Cours de formation générale\n
+        +   : Cours mineur\n
+        ( ) : Cours qui ne compte pas pour l'obtention de diplôme de Gestion\n
+        Semestres\n
+        1  : de septembre à fevrier\n
+        2  : de mars à juillet\n
+        Codes\n
+        Conditions de promotion :  La moyenne générale cumulative de 12 / 20 est\n
+        requise pour l'obtention du \"Bachellor Degree\".  Les cours transférés et\n
+        les cours complémentaires n'interviennent pas dans le calcul des moyennes,\n
+        ainsi que tout cours échoué, s'il est repris et réussi.  1 crédit\n
+        semestriel équivaut à 1.5 crédit trimestriel.  Le minimum de 132 crédits\n
+        semestriels requis pour avoir le Diplôme de Bachelor Degree équivaut à 198\n
+        crédits trimestriels.\n
+        ActiveReports Evaluation. Copyright 2002-2005 (c) Data Dynamics, Ltd. All Rights Reserved."
+}
+{
+    "pdfText": "\n\n2020-2021Année académique :\nPrinciples of Accounting I312.751ACCT 112  * \nMathématiques de l'Informatique312.101AMAT 111  \nMéthodes d'Étude Et De Recherche216.201EDRM 113  * \nGeneral English318.201ENGL 115  * \nMicrocomputer Applications315.001INSY 116  * \nIntroduction à l étude de la Bible217.501RELB 116  * \nStatistiques Descriptive314.402STAT 122  * \nIntroduction to Computer Programming418.002INSY 227  \nLogic et Circuits fondamentaux313.402MATH 127  \nPrincipes De Gestion39.422MGMT 124EC \nHealth Principles218.452HELT 213  * \nEntretien Des Ordinateurs312.702INSY 214  \n2021-2022Année académique :\nSystème de Gestion de Base  des Données31INSY 217 \nProgrammation avec C41INSY 228 \nStatistiques et Probabilités31STAT 223 \nBible Doctrines31RELT 123* \nUNIVERSITE  ADVENTISTE D'AFRIQUE CENTRALE\nB.P.  2461 KIGALI, RWANDA\nRELEVE DE NOTES\n23725Rusine Iriza CarmellaNom\nFaculté\nNo. Matricule\nOption\nMineur \nInformatique de GestionTechnologie de l' Information\n:\n::\n:\nSemSigleCoursCr .NoteCodes\nActiveReports Evaluation. Copyright 2002-2005 (c) Data Dynamics, Ltd. All Rights Reserved.\n\nUNIVERSITE  ADVENTISTE D'AFRIQUE CENTRALE\nB.P.  2461 KIGALI, RWANDA\nRELEVE DE NOTES\n23725Rusine Iriza CarmellaNom\nFaculté\nNo. Matricule\nOption\nMineur \nInformatique de GestionTechnologie de l' Information\n:\n::\n:\nSemSigleCoursCr .NoteCodes\nUNOFFICIAL\nRépartition des crédits\nCours généraux\nCours d'option\nCours du mineur\nCours transférés\nCours complémentaires\n:\n:\n:\n:\n:\n0\n21\n22\n0\n0\nAt Kigali, on\nMboyo Mukunde Sylvie\n13/01/2022\n14.71\n13.43\nMoyennes\nMoyenne générale cumulative\nMoyenne d'option cumulative\n:\n:\n    : Cours réussi\nEC  : Cours échoué\nTF  : Cours transféré\nW   : Cours retiré\n*   : Cours de formation générale\n+   : Cours mineur\n( ) : Cours qui ne compte pas pour l'obtention de diplôme de Gestion\nSemestres\n1  : de septembre à fevrier\n2  : de mars à juillet\nCodes\nConditions de promotion :  La moyenne générale cumulative de 12 / 20 est\nrequise pour l'obtention du \"Bachellor Degree\".  Les cours transférés et\nles cours complémentaires n'interviennent pas dans le calcul des moyennes,\nainsi que tout cours échoué, s'il est repris et réussi.  1 crédit\nsemestriel équivaut à 1.5 crédit trimestriel.  Le minimum de 132 crédits\nsemestriels requis pour avoir le Diplôme de Bachelor Degree équivaut à 198\ncrédits trimestriels.\nActiveReports Evaluation. Copyright 2002-2005 (c) Data Dynamics, Ltd. All Rights Reserved."
+}
